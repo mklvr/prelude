4f55b35dcad3c1f958bc9e7b476ec60db74ebbed
Mike Oliver <mike@mklvr.io>
2019-08-07 22:20:37 -0500

    Updates to theme handling and adding some packages.

---
+++ personal/preload/load-theme.el
@@ -0,0 +1,5 @@
+(when (string= system-name "saxifragum")
+  (setq prelude-theme 'leuven))
+
+(when (string= system-name "carbon01.office.resultsgeneration.com")
+  (setq prelude-theme 'dracula))
---
+++ personal/themes.el
@@ -0,0 +1,8 @@
+(require 'prelude-programming)
+(prelude-require-packages '(apropospriate-theme
+                            dracula-theme
+                            leuven-theme
+                            moe-theme
+                            tao-theme))
+
+
