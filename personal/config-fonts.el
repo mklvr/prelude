4e4a69579feb65bba012e427dd57d8731c67a8e5
Mike Oliver <mike@forge.undermountain.net>
2019-08-11 22:18:39 -0500

    Set the font for forge.

--- personal/config-fonts.el
+++ personal/config-fonts.el
@@ -1,3 +1,6 @@
 ;; Set the default font and size
 (when (string= (system-name) "carbon01.office.resultsgeneration.com")
   (set-default-font "Fira Mono-10"))
+
+(when (string= (system-name) "forge")
+  (set-default-font "Fira Code-10"))
