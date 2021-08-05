cee20e84a843cf9af57e281051771de7c07748a2
Mike Oliver <mike@flingtoad.com>
2021-08-05 12:35:37 -0500

    Add vterm back in here.

--- personal/config-term.el
+++ personal/config-term.el
@@ -1,6 +1,6 @@
 (require 'prelude-programming)
 
-(prelude-require-package 'multi-term)
+(prelude-require-packages '(multi-term multi-vterm vterm))
 
 (setq explicit-shell-file-name "/bin/zsh")
 (setq multi-term-dedicated-select-after-open-p t)
@@ -21,8 +21,23 @@
         (multi-term)
       (switch-to-buffer b))))
 
-(global-set-key (kbd "C-c '") 'get-term)
-(global-set-key (kbd "C-c \"") 'multi-term-dedicated-toggle)
+(defun get-vterm ()
+  "Switch to the term buffer last used, or create a new one if
+    none exists, or if the current buffer is already a term."
+  (interactive)
+  (let ((b (last-term-buffer (buffer-list))))
+    (if (or (not b) (eq 'vterm-mode major-mode))
+        (multi-vterm)
+      (switch-to-buffer b))))
+
+;; If we've gone through the trouble of installing vterm, use that instead.
+(if (require 'vterm nil :noerror)
+    (progn
+      (global-set-key (kbd "C-c '") 'get-vterm)
+      (global-set-key (kbd "C-c \"") 'multi-vterm-dedicated-toggle))
+  (progn
+    (global-set-key (kbd "C-c '") 'get-term)
+    (global-set-key (kbd "C-c \"") 'multi-term-dedicated-toggle)))
 
 (add-hook 'term-mode-hook (lambda ()
                             (define-key term-raw-map (kbd "C-c C-a") 'term-send-home)
