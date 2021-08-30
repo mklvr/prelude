5744a7e44831dd4095fd8688a70633b5b8ce952a
Mike Oliver <mike@flingtoad.com>
2021-08-30 14:22:19 -0500

    We don't use twittering mode again.

--- personal/config-twittering.el
+++
@@ -1,9 +0,0 @@
-;; config-twittering.el --- Load modules and configure twitter
-(require 'prelude-programming)
-
-(prelude-require-packages '(twittering-mode))
-
-(global-set-key (kbd "C-c C-n w") 'twit)
-
-(setq twittering-use-master-password t)
-(setq twittering-icon-mode nil)
